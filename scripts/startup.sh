34ff2bc460f231af585aa56830d549b45535d309
DannyDannyDanny <s144222@student.dtu.dk>
2018-10-23 11:03:35 +0200

    added stuff to startup script

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,5 +1,20 @@
 # run at linux startup.
 # updates and upgrades system
-sudo apt update; sudo apt upgrade -y
-cd
-#tmux
+#sudo apt update; sudo apt upgrade -y
+
+
+#OF=$(date +%Y%m%d)
+# make logfile for this week
+Min=$(date +%M)
+Hour=$(date +%H)
+Day=$(date +%a)
+Week=$(date +%V)
+Year=$(date +%Y)
+
+Logfilename=$Hour$Min-$Day-$Week-$Year.log
+
+echo making logfile $Logfilename
+touch ../logs/$Logfilename
+echo $(date +%H):$(date +%M):$(date +%S) log created >> ../logs/$Logfilename
+
+#rm rf ../logs/*
